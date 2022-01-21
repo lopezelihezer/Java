e0a35ae1f2901ce10ca4b025d26fb172a1e9db69
lopezelihezer <lopezelihezer@gmail.com>
2022-01-21 17:29:39 -0600

    compiling

--- files/Hello.java
+++ files/Hello.java
@@ -1,4 +1,7 @@
 public class Hello {
-    "Hello World!"
 
+public static void main (String[] args) {
+    // this is our main method, where we create the starting point for our program
+    System.out.println ("Hello World!");
+}
 }
